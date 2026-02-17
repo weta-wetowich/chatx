ab7d4b792e696a4ceab78fc1891006c1e209d0d2
weta-wetowich <mryarkiy1@gmail.com>
2026-02-17 09:10:52 +0400

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://fycubbmblarzllqgkgwy.supabase.co";
+  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5Y3ViYm1ibGFyemxscWdrZ3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyODk1NTAsImV4cCI6MjA4Njg2NTU1MH0.TF1HhoGFHDIar3vlySAqjutIJ1R3tnua-6kN632ggII";
+
+  window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+</script>
